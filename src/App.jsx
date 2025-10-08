3222b916e9ceecdca2412cb9c3c8147505f7dd30
Lucas Rocha <Lucasrocha03.2003@gmail.com>
2025-10-08 17:50:28 -0300

    Criação do cadastro de formulário, adaptação do HTML pra react e carrossel de imagens

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,203 @@
 import { useState } from 'react'
 import './App.css'
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import CadastroFormulario from './componentes/CadastroFormulario.jsx';
+
 function App() {
+
+  const [displayFormVoluntario, setDisplayFormVoluntario] = useState(false);
 
   return (
     <>
-     
+      {/* cabeçalho */}
+
+      <header className="cabecalho">
+        <div className="cabecalho-logo">
+          <img src="../src/img/logo.png" id="logo" />
+        </div>
+
+        <nav>
+          <a href="#quem-somos">Quem Somos</a>
+          <a href="">Ações</a>
+          <a href="">Missões & Valores</a>
+          <a href="">Voluntarie-se</a>
+          <a href="" id="doe">Doar</a>
+        </nav>
+      </header>
+      <main>
+
+        {/* inicio */}
+
+        <section className="transformando">
+          <div className="transformando-todo">
+            <div className="transformando-texto">
+              <h2>Transformamos <br /> <span id="vulnerabilidade">Vulnerabilidade</span> em <br />Esperança</h2>
+
+              <p id="transformando-info">Há quase 3 anos, o Coração Quentinho atua no Recife, levando refeições, cestas básicas, cadeiras de rodas, aulas de artesanato e apoio socioemocional para crianças. Somos movidos pela solidariedade e pelo amor ao próximo.
+              </p>
+
+
+            </div>
+          </div>
+          {/* imagem */}
+
+          <div className="imagem">
+            <Swiper
+              modules={[Navigation]}
+              navigation
+              loop={true}
+              spaceBetween={50}
+              slidesPerView={1}
+              className='img'
+            >
+              <SwiperSlide><img className='imgCarrossel' src="../src/img/imagem.png" /></SwiperSlide>
+              <SwiperSlide><img className='imgCarrossel' src="../src/img/carrossel1.jpg" /></SwiperSlide>
+              <SwiperSlide><img className='imgCarrossel' src="../src/img/carrossel2.jpg" /></SwiperSlide>
+            </Swiper>
+          </div>
+        </section>
+
+        <section className="quem-somos">
+          <h2>Quem somos</h2>
+          <div>
+            <p>O Coração Quentinho é uma ONG formada por voluntários que distribuem jantas para pessoas em situação de rua,
+              doam alimentos, cadeiras de rodas e de banho, e promovem aulas de artesanato e apoio socioemocional.
+              Nosso trabalho é 100% voluntário e baseado no amor ao próximo.
+            </p>
+          </div>
+        </section>
+
+
+        <section className="cards">
+          <div className="missao">
+            <h2>Missão</h2>
+            <p>Assistir pessoas em situação de vulnerabilidade,<br /> com foco no acolhimento,
+              desenvolvimento humano e respeito, através de doações e ações sociais.
+            </p>
+          </div>
+
+          <div className="visao">
+            <h2>Visão</h2>
+            <p>Ser referência em acolhimento e <br /> desenvolvimento humano,
+              reconhecida pelo impacto positivo nas comunidades atendidas.
+            </p>
+          </div>
+
+          <div className="valores">
+            <h2>Valores</h2>
+            <p>Ética, dignidade humana, cidadania, empatia, solidariedade e integridade.</p>
+          </div>
+        </section>
+
+        <section className="acoes">
+          <h2>Nossas Ações</h2>
+          <p>Conheça os principais projetos que transformam realidades e impactam vidas.</p>
+
+          <div className="acoes-cards">
+            <div className="acoes-1">
+
+              <div className="entrega">
+                <h2>Entrega de Refeições</h2>
+                <p>Distribuição de jantas para pessoas em situação de rua.</p>
+              </div>
+
+              <div className="cestas">
+                <h2>Cestas Básicas</h2>
+                <p>Entrega de alimentos e itens essenciais às famílias vulneráveis.</p>
+              </div>
+
+              <div className="aulas">
+                <h2>Aulas de Artesanato</h2>
+                <p>Oficinas para meninas do Bongi, incentivando o empreendedorismo.</p>
+              </div>
+
+
+            </div>
+
+            <div className="acoes-2">
+
+              <div className="cadeiras">
+                <h2>Doação de Cadeiras</h2>
+                <p>Cadeiras de rodas e de banho para quem necessita.</p>
+              </div>
+
+              <div className="socioemocional">
+                <h2>Apoio Socioemocional</h2>
+                <p>Estudos e debates com crianças, usando o livro 'Eu Lidero'.</p>
+              </div>
+
+            </div>
+
+          </div>
+        </section>
+        <section className="voluntario">
+          <h2>Voluntarie-se</h2>
+          <p>Ser voluntário no Coração Quentinho é aprender, crescer, transformar vidas e viver a empatia de forma prática.</p>
+
+          <div className="vantagens">
+            <div className="parte1">
+              <ul> Aprender algo novo</ul>
+              <ul> Promover o bem-estar</ul>
+              <ul>Fortalecer o emocional</ul>
+            </div>
+            <div className="parte2">
+
+              <ul>Ampliar a visão de mundo</ul>
+              <ul>Abrir portas para novas oportunidades</ul>
+              <ul>Transformar vidas e tornar o mundo melhor</ul>
+            </div>
+          </div>
+
+          <button onClick={() => setDisplayFormVoluntario(!displayFormVoluntario)}>Quero ser voluntário</button>
+        </section>
+
+        {/* apoie nosso trabalho */}
+
+        <section className="apoio">
+          <h2>Apoie a Nossa Causa</h2>
+          <p>Sua contribuição mantém nossos projetos vivos e impactando vidas todos os dias.</p>
+
+          <div className="pix">
+            <div className="pix-texto">
+              <p>PIX para doações:</p>
+            </div>
+            <div className="pix-qrcode">
+              <img src="../src/img/qrcode.png" alt="pix doações" />
+              <p>Titular: Gilma Galvão Mendes</p>
+            </div>
+
+          </div>
+          <a href="https://www.instagram.com/ongcoracaoquentinhorecife/?igsh=d2Q4bjF6eHR3dzd6#" target='_blank'>
+            <img src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/instagram-white-icon.png" alt="instagram" id="ig" />Instagram
+          </a>
+        </section>
+
+        {/* localização e redes sociais */}
+
+        <section className="saiba-mais">
+          <div className="saiba-mais-todo">
+            <div className="localizacao">
+              <h2>Nossa Localização</h2>
+              <p>Recife - PE - ONG Voluntária - Desde 2020</p>
+            </div>
+            <div className="redes">
+              <h2>Nossas Redes Sociais</h2>
+              <p>
+                <img src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/instagram-white-icon.png" />@ongcoracaoquentinho</p>
+              <img src="../src/img/ig_qrcode.png" id="ig_qrcode" alt="qrcode instagram" />
+            </div>
+          </div>
+          <footer className="rodape">
+
+          </footer>
+        </section>
+
+      </main>
+        {displayFormVoluntario ? <CadastroFormulario setDisplayFormVoluntario={setDisplayFormVoluntario}/> : null}
     </>
   )
 }
---
+++ src/componentes/CadastroFormulario.jsx
@@ -0,0 +1,137 @@
+import { useState, useEffect } from "react";
+
+import { X } from 'lucide-react';
+import './CadastroFormulario.css';
+
+function CadastroFormulario(props) {
+    const [nomeCompleto, setNomeCompleto] = useState('');
+    const [email, setEmail] = useState('');
+    const [telefone, setTelefone] = useState('');
+    const [observacoes, setObservacoes] = useState('');
+    const [enviado, setEnviado] = useState(false);
+
+    const [nomeCompletoError, setNomeCompletoError] = useState(false);
+    const [emailError, setEmailError] = useState(false)
+    const [telefoneError, setTelefoneError] = useState(false)
+
+    const [displaySubmitError, setDisplaySubmitError] = useState(false);
+    const [submitError, setSubmitError] = useState('');
+
+    useEffect(() => {
+        const nomeRegex = /^[A-Za-zÀ-ÖØ-öø-ÿ\s]+$/;
+        setNomeCompletoError(!nomeRegex.test(nomeCompleto));
+    }, [nomeCompleto]);
+
+    useEffect(() => {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        setEmailError(!emailRegex.test(email));
+    }, [email]);
+
+    useEffect(() => {
+        const telefoneRegex = /^(?:\+55\s?)?(?:\(?\d{2}\)?\s?)?(?:\d{4,5}-?\d{4})$/;
+        setTelefoneError(!telefoneRegex.test(telefone));
+    }, [telefone]);
+
+
+    async function enviarFormulario() {
+        if(nomeCompletoError === true || emailError === true || telefoneError === true){
+            setDisplaySubmitError(true);
+            setSubmitError('Todos os campos devem ser validos');
+            return;
+        }
+
+        if (enviado === true) {
+            setDisplaySubmitError(true)
+            setSubmitError('Esse formulário já foi enviado, recarregue a página ou tente novamente');
+            return;
+        }
+        console.log('Enviado:' + enviado);
+        console.log('nomeCompleto:' + nomeCompleto);
+        console.log('email:' + email);
+        console.log('telefone:' + telefone);
+        console.log('observacoes:' + observacoes);
+        try {
+            const response = await fetch('https://coracao-quentinho-ong-production.up.railway.app/voluntarios', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    nomeCompleto,
+                    email,
+                    telefone,
+                    detalhes: observacoes
+                })
+            });
+            if (response.ok) {
+                const result = await response.json()
+                console.log(result);
+                setEnviado(true);
+                setDisplaySubmitError(false);
+                setSubmitError('');
+                props.setDisplayFormVoluntario(false)
+            } else if (response.status === 403) {
+                setDisplaySubmitError(true);
+                setSubmitError('Esse email já foi cadastrado');
+                setEnviado(false);
+                return;
+            }
+        } catch (err) {
+            console.log(err);
+            setDisplaySubmitError(true);
+            setSubmitError("Ocorreu um erro Inesperado. Tente novamente mais tarde");
+            setEnviado(false);
+        }
+        setNomeCompleto('');
+        setEmail('');
+        setTelefone('');
+        setObservacoes('');
+        setSubmitError('');
+        setEnviado(false);
+    }
+
+    return (
+        <div id="formCadastroVoluntarioContainer">
+            <form action="" id="formCadastroVoluntario">
+                <button className="closeFormButton" onClick={(e) => {
+                    e.preventDefault();
+                    props.setDisplayFormVoluntario(false);
+                }}><X /></button>
+
+                <h1 className="titleFormulario">Cadastro de Voluntários</h1>
+
+                <div id="formCadastroVoluntario-inputContainer">
+                    <input type="text" className="cadastroVoluntario-input" placeholder="Nome Completo*" value={nomeCompleto} onChange={(e) => {
+                        setNomeCompleto(e.target.value);
+
+                    }} required />
+                    <p className="errorMessageInput" style={nomeCompletoError ? { color: 'red' } : { color: 'green' }}>O nome só pode conter letras e espaços.</p>
+
+                    <input type="email" className="cadastroVoluntario-input" placeholder="Email*" value={email} onChange={(e) => {
+                        setEmail(e.target.value);
+
+                    }} required />
+                    <p className="errorMessageInput" style={emailError ? { color: 'red' } : { color: 'green' }}>Insira um email válido.</p>
+
+                    <input type="text" className="cadastroVoluntario-input" placeholder="Telefone" value={telefone} onChange={(e) => {
+                        setTelefone(e.target.value);
+
+                    }} />
+                    <p className="errorMessageInput" style={telefoneError ? { color: 'red' } : { color: 'green' }}>Insira um telefone válido.</p>
+
+                    <input type="text" className="cadastroVoluntario-input" placeholder="Disponibillidade/Observações" value={observacoes} onChange={(e) => {
+                        setObservacoes(e.target.value);
+                    }} />
+                </div>
+                <p className="submitError">{displaySubmitError ? submitError : null}</p>
+                <button id="cadastroVoluntarioSubmit" onClick={(e) => {
+                    e.preventDefault()
+                    enviarFormulario()
+                }}>
+                    Cadastrar
+                </button>
+            </form>
+        </div>
+    );
+}
+export default CadastroFormulario;
