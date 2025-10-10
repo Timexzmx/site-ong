3ba842e750f10a2708a7893bfdc971383773409d
rafazxk <rs2789115@gmail.com>
2025-10-09 21:37:11 -0300

    organização da responsividade

--- src/App.jsx
+++ src/App.jsx
@@ -35,9 +35,9 @@ function App() {
         <section className="transformando">
           <div className="transformando-todo">
             <div className="transformando-texto">
-              <h2>Transformamos <br /> <span id="vulnerabilidade">Vulnerabilidade</span> em <br />Esperança</h2>
+              <h2>Transformamos <br /> <span className="vulnerabilidade">Vulnerabilidade</span> em <br />Esperança</h2>
 
-              <p id="transformando-info">Há quase 3 anos, o Coração Quentinho atua no Recife, levando refeições, cestas básicas, cadeiras de rodas, aulas de artesanato e apoio socioemocional para crianças. Somos movidos pela solidariedade e pelo amor ao próximo.
+              <p className="transformando-info">Há quase 3 anos, o Coração Quentinho atua no Recife, levando refeições, cestas básicas, cadeiras de rodas, aulas de artesanato e apoio socioemocional para crianças. Somos movidos pela solidariedade e pelo amor ao próximo.
               </p>
 
 
