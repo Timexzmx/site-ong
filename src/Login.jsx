a3f2967b0791ba44240a4d445558aba3006eb8e8
Lucas Rocha <Lucasrocha03.2003@gmail.com>
2025-10-18 12:01:25 -0300

    Editar, Deletar, Logout, Pesquisar, restrição de acesso concluidos

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,56 +1,67 @@
 import { useState, useEffect } from 'react';
-import {Eye, EyeOff} from 'lucide-react';
-import {useNavigate} from 'react-router-dom';
+import { Eye, EyeOff } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
 import Message from './componentes/Message.jsx';
 
 import './Login.css';
 
 //PRECISA FAZER UM useEffect PRA VERIFICAR SE O USUÁRIO ESTÁ LOGADO
 
+
 function Login() {
     
     const [passwordVisibility, setPasswordVisibility] = useState(false);
     const [loginError, setLoginError] = useState(null);
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const navigate = useNavigate()
-
-    const [displayMessage, setDisplayMessage] = useState(false)
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+    
+    const [displayMessage, setDisplayMessage] = useState(false);
     const [isMessageError, setIsMessageError] = useState(false);
-    const [message, setMessage] = useState('')
+    const [message, setMessage] = useState('');
+    
+    useEffect(() => {
+    const token = localStorage.getItem("token");
 
-    async function submitLogin(){
-        try{
-        if(!email.trim() || !password.trim()){
-            setLoginError('Todos os campos devem ser preenchidos');
-            return
-        }
-
-
-        const response = await fetch('https://coracao-quentinho-ong-production.up.railway.app/auth/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({username: email, password: password})
-        });
-        if(response.ok){
-            navigate('/admin');
-        }else{
-            const result = await response.json();
-            console.log(result);
-            setIsMessageError(true);
-            setMessage(result.error);
-            setDisplayMessage(true);
-            setLoginError('')
-            return
-        }
-    }catch(err){
-        console.log(err.message);
-        setIsMessageError(true);
-        setMessage(err.message);
-        setDisplayMessage(true);
+    if (token && token.startsWith("Bearer ")) {
+      navigate("/admin");
     }
+  }, [navigate]);
+
+    async function submitLogin() {
+        try {
+            if (!email.trim() || !password.trim()) {
+                setLoginError('Todos os campos devem ser preenchidos');
+                return
+            }
+
+
+            const response = await fetch('https://coracao-quentinho-ong-production.up.railway.app/auth/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ username: email, password: password })
+            });
+            if (response.ok) {
+                const result = await response.json();
+                localStorage.setItem('token', result.token)
+                navigate('/admin');
+            } else {
+                const result = await response.json();
+                console.log(result);
+                setIsMessageError(true);
+                setMessage(result.error);
+                setDisplayMessage(true);
+                setLoginError('')
+                return
+            }
+        } catch (err) {
+            console.log(err.message);
+            setIsMessageError(true);
+            setMessage(err.message);
+            setDisplayMessage(true);
+        }
     }
 
     return (
@@ -60,21 +71,29 @@ function Login() {
                 <div className="inputContainer">
                     <input type="email" className="input" placeholder='Digite seu email' value={email} onChange={(e) => {
                         setEmail(e.target.value);
-                    }} required/>
+                    }} required onKeyDown={(e) => {
+                        if (e.key === 'Enter') {
+                            submitLogin();
+                        }
+                    }} />
                     <div className="passwordContainer">
                         <input type={passwordVisibility ? "text" : "password"} className="input passwordInput" placeholder='Digite sua senha' value={password} onChange={(e) => {
                             setPassword(e.target.value);
-                        }} required/>
-                        <button type='button' className="passwordVisibilityButton" onClick={()=> {
+                        }} required onKeyDown={(e) => {
+                            if (e.key === 'Enter') {
+                                submitLogin();
+                            }
+                        }} />
+                        <button type='button' className="passwordVisibilityButton" onClick={() => {
                             setPasswordVisibility(!passwordVisibility);
-                        }}>{passwordVisibility ? <Eye className='eyeIcon'/> : <EyeOff className='eyeIcon'/>}</button>
+                        }}>{passwordVisibility ? <Eye className='eyeIcon' /> : <EyeOff className='eyeIcon' />}</button>
                     </div>
                 </div>
                 <div className="buttonSubmitContainer">
                     <p id="loginError">{loginError}</p>
-                    <button className="loginSubmitButton" onClick={submitLogin}>Entrar</button>
+                    <button className="loginSubmitButton" onClick={submitLogin} >Entrar</button>
                     <a href="#" id="esqueciASenha">Esqueci a Senha</a>
-                    </div>
+                </div>
             </div>
             {displayMessage ? <Message setDisplayMessage={setDisplayMessage} message={message} isMessageError={isMessageError} /> : null}
         </div>
---
+++ src/componentes/editarVoluntario.jsx
@@ -0,0 +1,185 @@
+import { useState, useEffect } from 'react';
+
+import { X, SquarePen } from 'lucide-react';
+import './editarVoluntario.css';
+
+function EditarVoluntario({ voluntarioID, setDisplayEditarVoluntario, setMessage, setDisplayMessage, setIsMessageError, voluntarios, setVoluntarios }) {
+
+
+    let oldVoluntario = voluntarios.find((v) => { return v.id == voluntarioID });
+    if (!oldVoluntario.observacao) {
+        oldVoluntario.observacao = '';
+    }
+    // console.log(oldVoluntario)
+
+    const [nomeCompletoError, setNomeCompletoError] = useState(false);
+    const [emailError, setEmailError] = useState(false)
+    const [telefoneError, setTelefoneError] = useState(false)
+
+    const [enviado, setEnviado] = useState(false);
+
+    //dos input
+    const [newNomeCompleto, setNewNomeCompleto] = useState(oldVoluntario.nome);
+    const [newEmail, setNewEmail] = useState(oldVoluntario.email);
+    const [newTelefone, setNewTelefone] = useState(oldVoluntario.telefone);
+    const [newObservacao, setNewObservacao] = useState(oldVoluntario.observacao);
+
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const token = localStorage.getItem('token');
+
+    useEffect(() => {
+        const nomeRegex = /^[A-Za-zÀ-ÖØ-öø-ÿ\s]+$/;
+        setNomeCompletoError(!nomeRegex.test(newNomeCompleto));
+    }, [newNomeCompleto]);
+
+    useEffect(() => {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        setEmailError(!emailRegex.test(newEmail));
+    }, [newEmail]);
+
+    useEffect(() => {
+        const telefoneRegex = /^(?:\+55\s?)?(?:\(?\d{2}\)?\s?)?(?:\d{4,5}-?\d{4})$/;
+        setTelefoneError(!telefoneRegex.test(newTelefone));
+    }, [newTelefone]);
+    async function enviarEditarVoluntario() {
+        try {
+
+            if (oldVoluntario.nome === newNomeCompleto &&
+                oldVoluntario.email === newEmail &&
+                oldVoluntario.telefone === newTelefone &&
+                oldVoluntario.observacao === newObservacao) {
+
+                setErrorMessage('Nenhuma alteração detectada!');
+                return;
+            }
+
+            if (nomeCompletoError === true || emailError === true || telefoneError === true) {
+                setErrorMessage('Todos os campos devem ser validos');
+                return;
+            }
+
+            if (enviado === true) {
+                setErrorMessage('Esse formulário já foi enviado, recarregue a página ou tente novamente');
+                return;
+            }
+
+            console.log('Token: ' + token);
+            const response = await fetch(`https://coracao-quentinho-ong-production.up.railway.app/voluntarios/atualizar/${voluntarioID}`, {
+                method: 'put',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': token
+                },
+                body: JSON.stringify({
+                    nomeCompleto: newNomeCompleto,
+                    email: newEmail,
+                    telefone: newTelefone,
+                    detalhes: newObservacao
+                })
+            });
+            console.log(response.ok);
+            if (response.ok) {
+
+                const newVoluntarios = voluntarios.map((v) => {
+                    if (v.id !== voluntarioID) {
+                        return v
+                    } else {
+                        return {
+                            ...v,
+                            nome: newNomeCompleto,
+                            email: newEmail,
+                            telefone: newTelefone,
+                            observacao: newObservacao || ''
+                        }
+                    }
+                })
+                console.log("newVoluntarios: ");
+                console.log(newVoluntarios);
+                setVoluntarios(newVoluntarios);
+                setMessage("Dados Alterados");
+                setIsMessageError(false);
+                setDisplayEditarVoluntario(false);
+                setDisplayMessage(true);
+            } else {
+                const result = await response.json()
+                console.log(result);
+                setMessage("Ocorreu um erro ao alterar os dados");
+                setIsMessageError(true);
+                setDisplayEditarVoluntario(false);
+                setDisplayMessage(true);
+            }
+        } catch (err) {
+            console.log(err);
+            setMessage("Ocorreu um erro ao alterar os dados");
+            setIsMessageError(true);
+            setDisplayEditarVoluntario(false);
+            setDisplayMessage(true);
+        }
+    }
+
+    return (
+        <div id='editarVoluntarioContainerMaxPro'>
+            <form id="editarVoluntario">
+                <button id="closeButton" onClick={() => {
+                    setDisplayEditarVoluntario(false);
+                }}><X /></button>
+                <h1 id="titleEditar">Editar Voluntário</h1>
+                <div id="voluntarioDataContainer">
+                    <div className="inputContainerVoluntario">
+                        <label className="labelVoluntario">
+                            Nome</label>
+                        <input type="text" className="inputVoluntario" value={newNomeCompleto} onChange={(e) => {
+                            setNewNomeCompleto(e.target.value);
+                        }} required onKeyDown={(e) => {
+                            if (e.key === 'Enter') {
+                                enviarEditarVoluntario();
+                            }
+                        }} />
+                        <p className="errorMessageInputEditar" style={nomeCompletoError ? { color: 'red' } : { color: 'green' }}>O nome só pode conter letras e espaços.</p>
+                    </div>
+                    <div className="inputContainerVoluntario">
+                        <label className="labelVoluntario">
+                            Email</label>
+                        <input type="email" className="inputVoluntario" value={newEmail} onChange={(e) => {
+                            setNewEmail(e.target.value);
+                        }} required onKeyDown={(e) => {
+                            if (e.key === 'Enter') {
+                                enviarEditarVoluntario();
+                            }
+                        }} />
+                        <p className="errorMessageInputEditar" style={emailError ? { color: 'red' } : { color: 'green' }}>Insira um email válido.</p>
+                    </div>
+                    <div className="inputContainerVoluntario">
+                        <label className="labelVoluntario">
+                            Telefone</label>
+                        <input type="text" className="inputVoluntario" value={newTelefone} onChange={(e) => {
+                            setNewTelefone(e.target.value);
+                        }} required onKeyDown={(e) => {
+                            if (e.key === 'Enter') {
+                                enviarEditarVoluntario();
+                            }
+                        }} />
+                        <p className="errorMessageInputEditar" style={telefoneError ? { color: 'red' } : { color: 'green' }}>Insira um telefone válido.</p>
+                    </div>
+                    <div className="inputContainerVoluntario">
+                        <label className="labelVoluntario">
+                            Observação</label>
+                        <input type="text" className="inputVoluntario" value={newObservacao} onChange={(e) => {
+                            setNewObservacao(e.target.value);
+                        }} onKeyDown={(e) => {
+                            if (e.key === 'Enter') {
+                                enviarEditarVoluntario();
+                            }
+                        }} />
+                    </div>
+                </div>
+                <div id="submitButtonContainerEditar">
+                    <p id="errorMessageSubmit">{errorMessage}</p>
+                    <button id="submitButtonEditarVoluntario" type='button' onClick={enviarEditarVoluntario}><SquarePen /> Editar</button>
+                </div>
+            </form>
+        </div>
+    )
+}
+export default EditarVoluntario;
